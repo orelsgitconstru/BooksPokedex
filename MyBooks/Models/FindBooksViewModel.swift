9c01c733a57d189de593b39304df34decd08dcb4
orelsgit <o.zilberman@constru.ai>
2022-01-31 17:06:02 +0200

    send search books request works

---
+++ MyBooks/Models/FindBooksViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  MyBooks
+//
+//  Created by Orel Zilberman on 31/01/2022.
+//
+
+import Foundation
+
+struct FindBooksViewModel {
+    static let shared: FindBooksViewModel = FindBooksViewModel()
+    
+    
+    
+    
+    private init(){}
+}
